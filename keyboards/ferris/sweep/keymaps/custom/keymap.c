060ba5c621b120aeac0bc08092017af561079425
Leandro Vargas <leandro.m.vargas@pm.me>
2025-10-28 14:52:16 +0100

    Add missing numbers

--- keyboards/ferris/sweep/keymaps/custom/keymap.c
+++ keyboards/ferris/sweep/keymaps/custom/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_split_3x5_2(
         KC_TRNS,    KC_1,        KC_2,        KC_3,        ACCENT,            KC_HOME,        KC_PGDN,        KC_PGUP,       KC_BSPC,   KC_END,
         KC_TRNS,    KC_4,        KC_5,        KC_6,        HACEK,             KC_LEFT,        KC_DOWN,        KC_UP,         KC_RGHT,   KC_TRNS,
-		CW_TOGG,    KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,           KC_TRNS,        KC_DEL,         KC_TRNS,       KC_TRNS,   KC_TRNS,
+		CW_TOGG,    KC_7,        KC_8,        KC_9,        KC_0,              KC_TRNS,        KC_DEL,         KC_TRNS,       KC_TRNS,   KC_TRNS,
 		                                      KC_SPC,      MO(3),             KC_TRNS,        KC_ENT
 		),
     [2] = LAYOUT_split_3x5_2(
